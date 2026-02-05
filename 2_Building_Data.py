bb96b9ddee57d912023b85da094d37e3f50be36f
Dylan Siewert <dsiewert@umich.edu>
2026-02-05 15:43:33 -0500

    query issue?

--- 2_Building_Data.py
+++ 2_Building_Data.py
@@ -109,7 +109,7 @@ def get_total_energy_of_usetype(energy_type):
             [usage]
         FROM [dbo].[{energy_type}]
         WHERE [usetype] = '{building_info['usetype']}' 
-            AND YEAR([enddate]) = 2024
+            AND DATEPART(YEAR, [enddate]) = 2024
     """
     df = conn.query(query)
 
